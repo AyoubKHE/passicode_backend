5409635e8e7e95274474d460cfec9e7a213ffaab
Computer <itsayoubkheyar06@gmail.com>
2026-01-28 14:02:25 +0100

    modify env fetch method

--- app/Http/Controllers/Orders/OrderCreationController.php
+++ app/Http/Controllers/Orders/OrderCreationController.php
@@ -53,9 +53,9 @@ class OrderCreationController extends Controller
                 "amount" => $this->chargily_payment->amount,
                 "currency" => $this->chargily_payment->currency,
                 "description" => "Payment ID={$this->chargily_payment->id}",
-                "success_url" => env('APP_URL') . "/payment/success",
-                "failure_url" => env('APP_URL') . "/payment/failure",
-                "webhook_endpoint" => env('APP_URL') . "/api/chargilypay/webhook",
+                "success_url" => config('url') . "/payment/success",
+                "failure_url" => config('url') . "/payment/failure",
+                "webhook_endpoint" => config('url') . "/api/chargilypay/webhook",
             ]);
 
             if (!$this->checkout) {
